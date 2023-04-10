a83cd6bd9e54e0fd1b3012a34875c0bc01450f25
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-04-10 15:07:26 +0200

    Higlight the current rating addEventListener added

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const ratings = document.querySelector('ul');
+const buttonSubmit = document.querySelector('.card__button');
+let ratingValue;
+
+// prettier-ignore
+ratings.addEventListener('click', (e) => {
+  if (e.target.classList.contains('card__list-item')) {
+    
+    // Remove .active class from rating
+    document.querySelectorAll('li').forEach((item) => {
+      item.classList.remove('active');
+    });
+
+    // Adding .active class to the selected element
+    e.target.classList.add('active');
+  }
+});
+
+// buttonSubmit.addEventListener('click', () => {});
